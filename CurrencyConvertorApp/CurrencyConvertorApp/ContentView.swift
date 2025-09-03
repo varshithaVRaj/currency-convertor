40725754db4a9fec562f90e554ed46b7e7093c12
varshitha v <varshitha.vraj@winds.co.in>
2025-09-03 07:52:47 +0530

    adding initial files

---
+++ CurrencyConvertorApp/CurrencyConvertorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConvertorApp
+//
+//  Created by Varshitha VRaj on 03/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConvertorApp/CurrencyConvertorApp/CurrencyConvertorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorAppApp.swift
+//  CurrencyConvertorApp
+//
+//  Created by Varshitha VRaj on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
